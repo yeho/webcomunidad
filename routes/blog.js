dac7d7dc903375a0daf6b6b94c50dc1d17aec07d
kevin04c <kevincespedes2004@gmai.com>
2022-10-27 23:38:04 -0500

    Arregle las cards wssp y cambie los var por const

--- routes/blog.js
+++ routes/blog.js
@@ -1,37 +1,25 @@
-const express =  require("express");
+const express = require("express");
 router = express.Router();
 const mariadb = require("mariadb");
 
-const db_past = process.env.password
-
-
+const db_past = process.env.password;
 
 const pool = mariadb.createPool({
-  host: 'localhost', 
-  user:'Itscience', 
+  host: "localhost",
+  user: "Itscience",
   password: db_past,
-  database:"itscience"
+  database: "itscience",
 });
 
-
-
-
-
-
 router.get("/", async function (req, res, next) {
+  let conn = await pool.getConnection();
+  const info = await conn.query("SELECT * FROM posts");
 
-
-  let conn  = await pool.getConnection();
-  const info = await conn.query("SELECT * FROM posts")
-
-  if(info.length ==0){
-
-    return res.render("blog",{ posts:false})
+  if (info.length == 0) {
+    return res.render("blog", { posts: false });
   }
 
-
-   return res.render("blog", { posts:info });
-
+  return res.render("blog", { posts: info });
 });
 
 module.exports = router;
